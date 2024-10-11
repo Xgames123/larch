0568e1257e6feb2eff8610983892ff727465eb9d
ldev <ldev@ldev.eu.org>
2024-10-11 22:29:08 +0200

    working on asm

--- nna/tools/libnna/src/lib.rs
+++ nna/tools/libnna/src/lib.rs
@@ -1,133 +1,74 @@
-mod u4;
-pub use u4::*;
+mod usmol;
+pub use usmol::*;
 
-pub enum Reg{
+pub enum Reg {
     R0,
     R1,
     R2,
-    R3
+    R3,
 }
 
-pub enum Instruction{
-    Nop,
-    Brk,
-    Flf,
-    Clf,
-    Shl,
-    Shr,
-    //unassigned
-    Lim,
-    New,
-    Mer,
-    Mov,
-    Jms,
-    Jmp,
-    Xor,
-    Add,
-    Mul,
-    Cmp,
-    Gt,
-    Invalid,
-
-}
-///Type of the 2 arguments of an operation
-enum ArgsTy{
-    Split(ArgTy)
-    Immediate,
-}
-///Argument type of an operations
-enum ArgTy{
-    Reg,
-    Const(u2),
-}
-impl ArgTy{
-    pub const fn zero() -> Self{
-        Self::Const(u2::ZERO)
-    }
+///Argument type of an operation
+pub enum ArgTy {
+    None(),
+    Any(&'static str),
+    Reg(&'static str),
 }
 
-impl Instruction{
-    ///they type of arguments the operation wants
-    pub fn arg_types(&self) -> ArgsTy{
-        use Self::*;
-        match self{
-            Nop => ArgsTy::Split(Arg::zero(), Arg::zero()),
-            Brk => ArgsTy::Split(Arg::zero(), Arg::Const(u2::ONE)),
-            Flf => ArgsTy::Split(Arg::zero(), Arg::Const(u2::TOW)),
-            Clf => ArgsTy::Split(Arg::zero(), Arg::Const(u2::THREE)),
-            Shl => ArgsTy::Split(Arg::Const(u2::ONE), Arg::Reg),
-            Shr => ArgsTy::Split(Arg::Const(u2::TOW), Arg::Reg),
+macro_rules! oparg {
+    (($desc:literal:reg)) => {
+        ArgTy::Reg($desc)
+    };
+    (($desc:literal:raw)) => {
+        ArgTy::Any($desc)
+    };
+    ((none)) => {
+        ArgTy::None()
+    };
+}
 
-            Lim => ArgsTy::Immediate,
-            Mew => ArgsTy::Immediate,
-            Mer => ArgsTy::Immediate,
-            Mov => ArgsTy::Split(Arg::Reg, Arg::Reg),
-            Jms => ArgsTy::Immediate,
-            Jmp => ArgsTy::Split(Arg::Reg, Arg::Reg),
-            Xor => ArgsTy::Split(Arg::Reg, Arg::Reg),
-            Add => 0x08,
-            Mul => 0x09,
-            Cmp => 0x0A,
-            Gt => 0x0B,
+macro_rules! ops {
+    ($vis:vis $name:ident{$($opname:literal:$opcode:literal,$arg0:tt,$arg1:tt),*}) => {
+        $vis struct $name(u8);
+        impl $name{
+            pub fn opcode(&self) -> u8{
+                self.0
+            }
+            pub fn arg_types(&self) -> ArgTy{
+                match self.0{
+                    $($opcode => oparg!($arg0)),*,
+                    _=>unreachable!(),
+                }
 
-            Invalid=>0x00,
+            }
+            pub fn try_from_str(string: &str) -> Option<Self>{
+                match string{
+                    $($opname => Some($name($opcode))),*,
+                    _=>None,
+                }
+            }
         }
-    }
-    pub fn opcode(&self) -> u4{
-        use Self::*;
-        u4::from_low(match self{
-            Nop => 0x00,
-            Brk => 0x00,
-            Flf => 0x00,
-            Clf => 0x00,
-            Shl => 0x00,
-            Shr => 0x00,
+    };
+}
+ops! {
+    pub NnaOp{
+        "nop":0x00,(none),(none),
+        "brk":0x01,(none),(none),
+        "flf":0x02,(none),(none),
+        "clf":0x03,(none),(none),
+        "shl":0x04,(none),("reg":reg),
+        "shr":0x08,(none),("reg":reg),
 
-            Lim => 0x01,
-            Mew => 0x02,
-            Mer => 0x03,
-            Mov => 0x04,
-            Jms => 0x05,
-            Jmp => 0x06,
-            Xor => 0x07,
-            Add => 0x08,
-            Mul => 0x09,
-            Cmp => 0x0A,
-            Gt => 0x0B,
-
-            Invalid=>0x00,
-
-        })
-    }
-    pub fn is_invalid(&self) -> bool{
-        match self{
-            Invalid => true,
-            _=>false,
-        }
-    }
-    pub fn try_from_str(string: &str) -> Self {
-        use Self::*;
-        match string {
-            "nop" => Nop,
-            "brk" => Brk,
-            "flf" => Flf,
-            "clf" => Clf,
-            "shl" => Shl,
-            "shr" => Shr,
-
-            "lim" => Lim,
-            "mew" => Mew,
-            "mer" => Mer,
-            "mov" => Mov,
-            "jms" => Jms,
-            "jmp" => Jmp,
-            "xor" => Xor,
-            "add" => Add,
-            "mul" => Mul,
-            "cmp" => Cmp,
-            "gt" => Gt,
-
-            _ => Invalid,
-        }
+        "lim":0x10,("value_low":raw),("value_high":raw),
+        "mew":0x20,("addr_low":raw), ("addr_high":raw),
+        "mer":0x30,("addr_low":raw), ("addr_high":raw),
+        "mov":0x40,("source":reg),   ("dest":reg),
+        "jms":0x50,("addr_low":raw), ("addr_high":raw),
+        "jmp":0x60,("addr":reg),     ("bank":reg),
+        "xor":0x70,("source":reg),   ("a":reg),
+        "add":0x80,("source":reg),   ("a":reg),
+        "mul":0x90,("source":reg),   ("a":reg),
+        "cmp":0xA0,("a":reg),        ("b":reg),
+        "gt" :0xB0,("a":reg),        ("b":reg)
     }
 }
